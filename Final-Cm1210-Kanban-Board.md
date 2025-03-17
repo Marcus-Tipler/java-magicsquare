06cbda2569c92367dd5bb04b115581740ed6154d
Marcus <TiplerMF@cardiff.ac.uk>
2025-03-17 03:18:40 +0000

    Steps 1 through 3 complete in q1b.

---
+++ Final-Cm1210-Kanban-Board.md
@@ -0,0 +1,56 @@
+---
+
+kanban-plugin: board
+
+---
+
+## Backlog
+
+- [ ] Q1A: Replace Exception Printouts with proper text based responses.
+- [ ] Debug that 1 is errored out and that negatives aren't accepted
+
+
+## Blocked
+
+- [ ] [[Create a function for the shuffling algorithm called Fisher_Yates. (edited for 2d arrays)]]
+- [ ] [[Turn the program in to a game. Make the user input a [ROW], a [COL] and a [DIR] that can be abbreviated.]]
+- [ ] Compare my work with the requirements folder
+- [ ] Implement q1a to q1b, only the catch error and execSquare functions
+
+
+## In Progress
+
+- [ ] [[Q1B Step 1 - Print instructions and Query user about size of array.]]
+- [ ] Q1B: Step 2 - Verify user input.
+- [ ] Q1B: Step 3 - Make finished square.
+- [ ] Q1B: Step 4 - Shuffle square using 'execShift' n\*\*2 times.
+- [ ] Q1B: Step 5 - Prompt user with instructions and printed square.
+- [ ] Q1B: Step 6 - Start a loop that queries and performs the users requested actions until the matrix is that of a magic square.
+
+
+## PR Ready
+
+- [ ] Comment all the code and what it achieves
+- [ ] [[Q1B New idea make a function that takes a row, column and direction.]]
+- [ ] [[Q1B New code structure for entire program]]
+
+
+## Done
+
+**Complete**
+- [x] Q1B: Import from q1a_24062219 for q1b_24062219
+- [x] [[New shuffling program to be made]]
+- [x] Create a new file called q1b_24062219
+- [x] 3. Set values of the matrix
+- [x] 2. Initialise the array for the magic square
+- [x] 1. Prompt the user for an Odd Integer Value
+- [x] [[Make the pseudocode for the assessment]]
+
+
+
+
+%% kanban:settings
+```
+{"kanban-plugin":"board","list-collapse":[false,false,false,false,false]}
+```
+%%
--- q1b_24062219.java
+++ q1b_24062219.java
@@ -1,7 +1,78 @@
+import java.util.Scanner;
+
 public class q1b_24062219 extends q1a_24062219 {
     public q1b_24062219() {
-        int n = 3;
-        int[][] execSquared = execSquare(n);
-        execPrint(execSquared, n);
+        super();
     }
+
+
+    public static void main(String[] args) {
+        // ----------------------------------------------------------------
+        // <STEP 1> Print instructions and Query user about size of array.
+        // <STEP 2> Verify user input.
+        // <STEP 3> Make finished square.
+        // <STEP 4> Shuffle square using 'execShift' n**2 times.
+	    // <STEP 5> Prompt user with instructions and printed square.
+        // <STEP 6> Start a loop that queries and performs the users
+        // requested actions until the matrix is that of a magic square.
+        // ----------------------------------------------------------------
+
+        String[] sizeOfMatrix = execStart(); 
+        int checkedSize = execVerify(sizeOfMatrix);
+        int[][] execSquared = execSquare(checkedSize); //FIXME: Whilst value is negative, loop and ask again.
+        // execPrint(execSquared, checkedSize);
+
+    }
+
+
+    // ----------------------------------------------------------------
+    // Print instructions and Query user about size of array.
+    // ----------------------------------------------------------------
+    static String[] execStart() {
+        System.out.println("Hello user! Welcome to the Magic Square Game!");
+        Scanner requestSizeOfMatrix = new Scanner(System.in);
+        System.out.println("Please enter an odd number for the size of the array (3, 5, 7, etc.): ");
+        String sizeOfMatrix = requestSizeOfMatrix.nextLine();
+        System.out.println("Matrix Size is: " + sizeOfMatrix);
+        requestSizeOfMatrix.close();
+        return new String[]{sizeOfMatrix};
+    }
+
+
+    // ----------------------------------------------------------------
+    // Shifts the matrix based on Column, Row and Direction data.
+    // ----------------------------------------------------------------
+    static int[][] execShift(int[][] square, int i, int j, String d) {
+        
+    }
+
+    
+    // ----------------------------------------------------------------
+    // Shuffle the matrix using RandInt and 'execShift' function.
+    // ----------------------------------------------------------------
+
+
+    // ----------------------------------------------------------------
+    // Prompt the user with instructions and the shuffled matrix.
+    // ----------------------------------------------------------------
+
+    
+    // ----------------------------------------------------------------
+    // Query the user about the shift they'd like to perform.
+    // ----------------------------------------------------------------
+
+
+    // ----------------------------------------------------------------
+    // Verify the user input for the shift they'd like to perform.
+    // ----------------------------------------------------------------
+
+    
+    // ----------------------------------------------------------------
+    // Verify the completion of the matrix to a completed Magic Square.
+    // ----------------------------------------------------------------
+
+
+    // ----------------------------------------------------------------
+    // Prints the end game results and text.
+    // ----------------------------------------------------------------
 }
